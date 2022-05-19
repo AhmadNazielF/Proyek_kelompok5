d148fb00076d81887e6fb14a863b1c5b58c35f39
Zaki Zamani <44667928+EKIZAMANI@users.noreply.github.com>
2022-05-19 12:38:19 +0700

    Marapikan Code
    meletakkan function menjadi di bawah main.

--- main.c
+++ main.c
@@ -6,8 +6,113 @@
 int lv = 1, pilihan;
 long int uang = 0;
 char jwb[4];
-// di deklarsikan agar tidak terjadi error
+
+// function declaration 
 void mulai();
+void menu();
+void level();
+void kembali();
+void benar();
+void GameOver();
+void soal1();
+void soal2();
+void soal3();
+void soal4();
+void soal5();
+void soal6();
+void soal7();
+void soal8();
+void soal9();
+void soal10();
+
+// fungsi Utama
+int main(int banyakArgumen, char *argumen[])
+{
+    int temp; // jika berhasil login temp = 1
+
+    // LOGIN
+    //  variabel untuk menampung username dan password
+    char login[20];
+
+    // jika banyak argumen 1, maka akan dilakukan registrasi akun
+    if (banyakArgumen == 1)
+    {
+        FILE *reg1 = fopen("login.bin", "wb");
+        printf("+----------------------------------------------------------+\n");
+        printf("|                 Registrasi Pembuatan Akun                |\n");
+        printf("+----------------------------------------------------------+\n");
+        printf("Masukkan username dan password mengikuti format berikut !\n");
+        printf("username@password : ");
+        scanf("%s", login);
+        fwrite(&login, sizeof(login), 1, reg1);
+        printf("+-----------------------------------------------------------+\n");
+        printf("| Cara Penggunaan : ./FileProgramUtama username password    |\n");
+        printf("+-----------------------------------------------------------+\n");
+        fclose(reg1);
+    }
+
+    // jika banyak argumen tidak 3 dan 1, makan program akan memberitahu user
+    // cara login yang benar.
+    if (banyakArgumen != 3 && banyakArgumen != 1)
+    {
+        printf("Gagal login !\n");
+        printf("+-----------------------------------------------------------+\n");
+        printf("| Cara Penggunaan : ./FileProgramUtaman username password   |\n");
+        printf("+-----------------------------------------------------------+\n");
+    }
+
+    // jika banyakArgummen 3, maka akan dijalankan login
+    if (banyakArgumen == 3)
+    {
+        char userInput[10], passInput[10];
+        strcpy(userInput, argumen[1]);
+        strcpy(passInput, argumen[2]);
+
+        FILE *reg2;
+
+        if ((reg2 = fopen("login.bin", "rb")) == NULL)
+        {
+            printf("Gagal membukan file !\n");
+            return EXIT_FAILURE;
+        }
+
+        char akun[20];
+        fread(akun, sizeof(char), sizeof(akun) / sizeof(char), reg2);
+
+        fclose(reg2);
+
+        char *string[3];
+        char username[20], password[20];
+        int ctrl = 0;
+
+        string[0] = strtok(akun, "@");
+        while (string[ctrl++] != NULL)
+        {
+            string[ctrl] = strtok(NULL, "@");
+        }
+
+        strcpy(username, string[0]);
+        strcpy(password, string[1]);
+
+        if ((strcmp(userInput, username) == 0) && (strcmp(passInput, password) == 0))
+        {
+            printf("\n       Selamat anda berhasil login !\n");
+            temp = 1;
+        }
+
+        fclose(reg2);
+    }
+
+    if (temp == 1)
+    { // temp = 1 artinya berhasil login
+        mulai();
+    }
+
+    return 0;
+}
+
+
+
 // menampilkan tampilan awal
 void menu()
 {
@@ -360,90 +465,4 @@ void mulai()
     {
         printf("Terimakasih Telah Berkunjung!");
     }
-}
-
-// fungsi Utama
-int main(int banyakArgumen, char *argumen[])
-{
-    int temp; // jika berhasil login temp = 1
-
-    // LOGIN
-    //  variabel untuk menampung username dan password
-    char login[20];
-
-    // jika banyak argumen 1, maka akan dilakukan registrasi akun
-    if (banyakArgumen == 1)
-    {
-        FILE *reg1 = fopen("login.bin", "wb");
-        printf("+----------------------------------------------------------+\n");
-        printf("|                 Registrasi Pembuatan Akun                |\n");
-        printf("+----------------------------------------------------------+\n");
-        printf("Masukkan username dan password mengikuti format berikut !\n");
-        printf("username@password : ");
-        scanf("%s", login);
-        fwrite(&login, sizeof(login), 1, reg1);
-        printf("+-----------------------------------------------------------+\n");
-        printf("| Cara Penggunaan : ./FileProgramUtama username password    |\n");
-        printf("+-----------------------------------------------------------+\n");
-        fclose(reg1);
-    }
-
-    // jika banyak argumen tidak 3 dan 1, makan program akan memberitahu user
-    // cara login yang benar.
-    if (banyakArgumen != 3 && banyakArgumen != 1)
-    {
-        printf("Gagal login !\n");
-        printf("+-----------------------------------------------------------+\n");
-        printf("| Cara Penggunaan : ./FileProgramUtaman username password   |\n");
-        printf("+-----------------------------------------------------------+\n");
-    }
-
-    // jika banyakArgummen 3, maka akan dijalankan login
-    if (banyakArgumen == 3)
-    {
-        char userInput[10], passInput[10];
-        strcpy(userInput, argumen[1]);
-        strcpy(passInput, argumen[2]);
-
-        FILE *reg2;
-
-        if ((reg2 = fopen("login.bin", "rb")) == NULL)
-        {
-            printf("Gagal membukan file !\n");
-            return EXIT_FAILURE;
-        }
-
-        char akun[20];
-        fread(akun, sizeof(char), sizeof(akun) / sizeof(char), reg2);
-
-        fclose(reg2);
-
-        char *string[3];
-        char username[20], password[20];
-        int ctrl = 0;
-
-        string[0] = strtok(akun, "@");
-        while (string[ctrl++] != NULL)
-        {
-            string[ctrl] = strtok(NULL, "@");
-        }
-
-        strcpy(username, string[0]);
-        strcpy(password, string[1]);
-
-        if ((strcmp(userInput, username) == 0) && (strcmp(passInput, password) == 0))
-        {
-            printf("\n       Selamat anda berhasil login !\n");
-            temp = 1;
-        }
-
-        fclose(reg2);
-    }
-
-    if (temp == 1)
-    { // temp = 1 artinya berhasil login
-        mulai();
-    }
-
-    return 0;
 }
